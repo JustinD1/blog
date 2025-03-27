35954020369195f97d383ccb5d8f49ef8fd3bae6
JustinD1 <donohoejustin+code@googlemail.com>
2025-03-27 21:54:14 +0000

    Setting up the auth for logging in for Admin user

---
+++ blog/src/api/auth.js
@@ -0,0 +1,8 @@
+import axios from "axios";
+
+const API_URL = import.meta.env.VITE_API_URL || "http://localhost:8080";
+
+export const loginUser = async (email, password) => {
+  const res = await axios.post(`${API_URL}/api/login`, { email, password });
+  return res.data;
+};
--- blog/src/main.jsx
+++ blog/src/main.jsx
@@ -1,10 +1,12 @@
-import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
+import {QueryClient, QueryClientContext} from "@tanstack/react-query";
+
+const queryClient = new QueryClient ();
 
 createRoot(document.getElementById('root')).render(
-  <StrictMode>
+  <QueryClientContext client={queryClient}>
     <App />
-  </StrictMode>,
+  </QueryClientContext>,
 )
