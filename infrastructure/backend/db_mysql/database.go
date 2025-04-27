5634705228d558eb39518efad31f2494ded412a9
JustinD1 <donohoejustin+code@googlemail.com>
2025-04-27 09:46:56 +0100

    Added missing models, util and latest sql migration for new post field
      - removed debug prints

--- infrastructure/backend/db_mysql/database.go
+++ infrastructure/backend/db_mysql/database.go
@@ -31,18 +31,14 @@ func ConnectDb () {
 		log.Fatal ("failed to connect to the database:", err)
 	}
 	DB = database
-	fmt.Println ("Connected to database")
 }
 
 func GetPost (limit, offset int) ([]models.PublicPost, error) {
-	log.Println ("database.go/GetPost")
 	var posts []models.PublicPost
 	var now = time.Now ().UTC ().Add (time.Hour)
-	log.Println ("now:", now)
 
 	query := "SELECT id, uuid, title, content, author, publish_at FROM posts WHERE publish_at < ? LIMIT ? OFFSET ?"
 	rows, err := DB.Query(query, now, limit, offset)
-	log.Println ("Rows:", rows)
 	if err != nil {
 		return nil, fmt.Errorf("error querying posts: %v", err)
 	}
---
+++ infrastructure/backend/models/post.go
@@ -0,0 +1,28 @@
+package models
+
+import (
+	"time"
+	"database/sql"
+)
+
+type Post struct {
+	ID        int          `json:"id"`
+	Uuid      string       `json:"uuid"`
+	Title     string       `json:"title"`
+	Content   string       `json:"content"`
+	Author    string       `json:"author"`
+	CreatedAt time.Time    `json:"created_at"`
+	PublishAt sql.NullTime `json:"publish_at"`
+	UpdatedAt sql.NullTime `json:"updated_at"`
+	Count     int          `json:"count"`
+	IsDraft   bool         `json:"is_draft"`
+}
+
+type PublicPost struct {
+	ID        int          `json:"id"`
+	Uuid      string       `json:"uuid"`
+	Title     string       `json:"title"`
+	Content   string       `json:"content"`
+	Author    string       `json:"author"`
+	PublishAt sql.NullTime `json:"publish_at"`
+}
